aaf59f952d8d86dbb9da58e0af8d59347715bd33
Gerardusdavidbayuaji <gerardusdavidbayu@gmail.com>
2023-11-27 01:44:19 +0700

    feat: pokemon app setup

---
+++ pokemon-app/src/pages/pokemon-app.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <Button className="bg-neutral-300">test</Button>
+    </div>
+  )
+}
+
+export default App;
