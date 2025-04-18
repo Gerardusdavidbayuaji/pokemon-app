8ae2726d200fac5891fedaeb3213ec2374cd2e02
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-04-18 20:19:30 +0700

    feat: update main page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
+
 import Header from "./Header";
 import Footer from "./Footer";
+import { Toaster } from "./ui/sonner";
 
 interface Props {
   children: ReactNode;
@@ -9,11 +11,12 @@ interface Props {
 const Layout = ({ children }: Props) => {
   return (
     <div className="bg-[#0d1d23] w-full h-screen flex justify-center">
-      <div className="w-1/3 flex flex-col rounded-md my-2 overflow-y-auto">
+      <div className="w-1/3 flex flex-col bg-white/10 backdrop-blur-sm rounded-md my-2 overflow-y-auto">
         <Header />
-        <main className="grow bg-white/10 backdrop-blur-sm text-center p-1">
+        <main className="grow text-center overflow-y-auto webkit-scrollbar">
           {children}
         </main>
+        <Toaster />
         <Footer />
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+import { getPokemon, Response } from "@/utils/pokemon";
+import { formatId } from "@/utils/formatter";
+
 import PokemonCard from "@/components/PokemonCard";
 import Layout from "@/components/Layout";
 
 const App = () => {
+  const [pokemons, setPokemons] = useState<Response>();
+
+  const fetchPokemonDatas = async () => {
+    try {
+      const response = await getPokemon();
+      setPokemons(response);
+    } catch (error: any) {
+      toast("Upss, something when wrong!", {
+        description: error.toString(),
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemonDatas();
+  }, []);
+
   return (
     <>
       <Layout>
-        <PokemonCard />
+        <div className="grid grid-cols-2 gap-2 p-2 overflow-y-auto">
+          {pokemons?.results.map((pokemon) => {
+            const id = formatId(pokemon.url);
+            const name = pokemon.name;
+            return <PokemonCard id={id} name={name} />;
+          })}
+        </div>
       </Layout>
     </>
   );
