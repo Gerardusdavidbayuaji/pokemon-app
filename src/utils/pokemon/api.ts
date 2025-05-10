f4a554d7ee755fb74bb38cd8801e9abd2d2bedfd
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-05-10 17:42:59 +0700

    feat: update api

--- src/utils/pokemon/api.ts
+++ src/utils/pokemon/api.ts
@@ -1,5 +1,5 @@
 import axiosWithConfig from "../axiosWithConfig";
-import { Request, Response } from ".";
+import { Request, Response, Pokemon } from ".";
 
 export const getPokemon = async (params?: Request) => {
   try {
@@ -20,6 +20,15 @@ export const getPokemon = async (params?: Request) => {
 
     const response = await axiosWithConfig.get(url);
     return response.data as Response;
+  } catch (error: any) {
+    throw Error(error.response.data.message);
+  }
+};
+
+export const getDetailPokemon = async (id: number) => {
+  try {
+    const response = await axiosWithConfig(`/pokemon/${id}`);
+    return response.data as Pokemon;
   } catch (error: any) {
     throw Error(error.response.data.message);
   }
