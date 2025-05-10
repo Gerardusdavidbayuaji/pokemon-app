da2f05bee9d8b7941a0e1a7ca6d985373bc9b2a4
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-05-10 20:04:16 +0700

    feat: update detail page

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 interface PokemonCardProps {
   id: string | undefined;
   name: string;
@@ -5,7 +7,10 @@ interface PokemonCardProps {
 
 const PokemonCard = ({ id, name }: PokemonCardProps) => {
   return (
-    <div className="bg-[#0d1d23]/50 border-2 border-white/20 rounded-md shadow-md flex flex-col justify-center items-center transition-all duration-300 scale-100 hover:scale-[1.03] overflow-hidden cursor-pointer mb-1">
+    <Link
+      to={`/pokemon/${id}`}
+      className="bg-[#0d1d23]/50 border-2 border-white/20 rounded-md shadow-md flex flex-col justify-center items-center transition-all duration-300 scale-100 hover:scale-[1.03] overflow-hidden cursor-pointer mb-1"
+    >
       <img
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
         alt={name}
@@ -14,7 +19,7 @@ const PokemonCard = ({ id, name }: PokemonCardProps) => {
       <div className="w-full bg-[#f4f3f1] text-[#161514] py-2">
         <p className="text-lg font-black capitalize">{name}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/detailPokemon.tsx
+++ src/pages/detailPokemon.tsx
@@ -1,95 +1,69 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 import { getDetailPokemon } from "@/utils/pokemon/api";
-import poke from "/assets/pokemon.png";
+import { formatTilte } from "@/utils/formatter";
+import { Pokemon } from "@/utils/pokemon";
 
 import { Progress } from "@/components/ui/progress";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import Layout from "@/components/Layout";
-import { Link } from "react-router-dom";
 
 const DetailPokemon = () => {
+  const [pokemon, setPokemon] = useState<Pokemon>();
   const params = useParams();
   const fetchDetailPokemon = async () => {
     try {
       const response = await getDetailPokemon(+params.id!);
-      console.log(response);
+      setPokemon(response);
     } catch (error) {}
   };
 
   useEffect(() => {
     fetchDetailPokemon();
-  });
+  }, []);
 
   return (
     <Layout>
       <div className="p-2">
-        {/* <Button className="bg-transparent mb-2 border border-white text-white px-2 flex justify-center items-center">
-          <ChevronLeft />
-          <p>Back</p>
-        </Button> */}
         <div className="grid grid-cols-2 p-2 gap-x-2">
           <div className="flex flex-col justify-between">
-            <img src={poke} alt="poke" className="w-auto h-full" />
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon?.id}.svg`}
+              alt={pokemon?.name}
+              className="w-auto h-full"
+            />
             <div className="space-x-1">
-              <Badge className="px-4 py-1 rounded-full bg-[#43AB92]">
-                Lorem
-              </Badge>
-              <Badge className="px-4 py-1 rounded-full bg-[#00AAA0]">
-                Ipsum
-              </Badge>
+              {pokemon?.types.map((data, index) => (
+                <Badge
+                  key={index}
+                  className="px-4 py-1 rounded-full bg-[#43AB92]"
+                >
+                  {data.type.name}
+                </Badge>
+              ))}
             </div>
           </div>
           <div>
             <h1 className="flex justify-start border-b-2 border-white/20 mb-2 font-medium text-2xl text-white pb-1 italic">
               Stats
             </h1>
-            <div className="flex flex-col mb-1 gap-y-1">
-              <div className="flex justify-between text-white/50">
-                <p className="font-normal text-base">Lorem</p>
-                <p>50</p>
+            {pokemon?.stats.map((data, index) => (
+              <div key={index} className="flex flex-col mb-1 gap-y-1">
+                <div className="flex justify-between text-white/50">
+                  <p className="font-normal text-base">
+                    {formatTilte(data.stat.name)}
+                  </p>
+                  <p>{data.base_stat}</p>
+                </div>
+                <Progress value={data.base_stat} className="h-[0.5rem]" />
               </div>
-              <Progress value={50} className="h-[0.5rem]" />
-            </div>
-            <div className="flex flex-col mb-1 gap-y-1">
-              <div className="flex justify-between text-white/50">
-                <p className="font-normal text-base">Lorem</p>
-                <p>50</p>
-              </div>
-              <Progress value={50} className="h-[0.5rem]" />
-            </div>
-            <div className="flex flex-col mb-1 gap-y-1">
-              <div className="flex justify-between text-white/50">
-                <p className="font-normal text-base">Lorem</p>
-                <p>50</p>
-              </div>
-              <Progress value={50} className="h-[0.5rem]" />
-            </div>
-            <div className="flex flex-col mb-1 gap-y-1">
-              <div className="flex justify-between text-white/50">
-                <p className="font-normal text-base">Lorem</p>
-                <p>50</p>
-              </div>
-              <Progress value={50} className="h-[0.5rem]" />
-            </div>
-            <div className="flex flex-col mb-1 gap-y-1">
-              <div className="flex justify-between text-white/50">
-                <p className="font-normal text-base">Lorem</p>
-                <p>50</p>
-              </div>
-              <Progress value={50} className="h-[0.5rem]" />
-            </div>
-            <div className="flex flex-col mb-1 gap-y-1">
-              <div className="flex justify-between text-white/50">
-                <p className="font-normal text-base">Lorem</p>
-                <p>50</p>
-              </div>
-              <Progress value={50} className="h-[0.5rem]" />
-            </div>
+            ))}
           </div>
         </div>
+
         <div className="grid grid-cols-2 p-2 gap-x-2">
           <div className="rounded-md p-2 border-2 border-white/20 bg-[#43AB9220] text-white">
             <h1 className="flex justify-start border-b-2 border-white/20 mb-2 font-medium text-2xl pb-1 italic">
@@ -98,16 +72,14 @@ const DetailPokemon = () => {
             <div className="grid grid-cols-2 gap-y-2">
               <p className="font-medium text-xl text-white/50">
                 Height <br />
-                <span className="font-normal text-lg">1 m</span>
+                <span className="font-normal text-lg">{pokemon?.height} m</span>
               </p>
               <p className="font-medium text-xl text-white/50">
                 Weight
                 <br />
-                <span className="font-normal text-lg">1 kg</span>
-              </p>
-              <p className="font-medium text-xl text-white/50">
-                Habitat <br />
-                <span className="font-normal text-lg">Klinnet Kos</span>
+                <span className="font-normal text-lg">
+                  {pokemon?.weight} kg
+                </span>
               </p>
             </div>
           </div>
@@ -115,8 +87,11 @@ const DetailPokemon = () => {
             <h1 className="flex w-full border-b-2 border-white/20 mb-2 font-medium text-2xl pb-1 italic">
               Abillity
             </h1>
-            <p className="font-medium text-xl text-white/50"> › Bersin</p>
-            <p className="font-medium text-xl text-white/50"> › Ngupil</p>
+            {pokemon?.abilities.map((data, index) => (
+              <p key={index} className="font-medium text-xl text-white/50">
+                › {formatTilte(data.ability.name)}
+              </p>
+            ))}
           </div>
         </div>
         <Link to="/catch">
